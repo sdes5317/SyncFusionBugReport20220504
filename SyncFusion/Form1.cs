dca212cc0a737f921ca50d401dfe4f19dd1b1ff3
Gino Ko <ginok.twcp@gmail.com>
2022-05-04 09:24:06 +0800

    init

---
+++ SyncFusion/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SyncFusion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var pdfForm = new PdfForm();
+            pdfForm.Load("2.pdf");
+            pdfForm.PageTo(2);
+            pdfForm.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var pdfForm = new PdfForm();
+            pdfForm.Load("3.pdf");
+            pdfForm.PageTo(2);
+            pdfForm.Show();
+        }
+    }
+}
---
+++ SyncFusion/PdfForm.cs
@@ -0,0 +1,37 @@
+﻿using Syncfusion.Windows.Forms.PdfViewer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SyncFusion
+{
+    public partial class PdfForm : Form
+    {
+        private readonly PdfViewerControl pdfViewerControl;
+
+        public PdfForm()
+        {
+            InitializeComponent();
+
+            pdfViewerControl = new PdfViewerControl();
+            pdfViewerControl.Dock = DockStyle.Fill;
+            Controls.Add(pdfViewerControl);
+
+        }
+
+        public void Load(string path)
+        {
+            pdfViewerControl.Load(path);
+        }
+        public void PageTo(int page)
+        {
+            pdfViewerControl.GoToPageAtIndex(page);
+        }
+    }
+}
